9cc9fb32db5892deaf0313679a16506f1658ac66
Jonah Massam <Jonah.Massam10@yahoo.co.uk>
2022-01-18 15:38:33 +0000

    Q_10_done

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,6 +1,9 @@
 class DockingStation
   
   def release_bike()
+
+    new_bike = Bike.new
+    
   end
   
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,6 +1,13 @@
 require './lib/DockingStation.rb'
 describe DockingStation do 
-  
   it { is_expected.to respond_to :release_bike }
+end
 
+describe "Test" do
+  it "test" do
+    new_dock = DockingStation.new
+    new_bike = new_dock.release_bike
+    expect(new_bike).not_to eq(nil)
+    expect(new_bike).to respond_to(:working?)
+  end
 end
